ad6abb47c314e7eda2471f3275bf1d434992bc66
Tejas Bitale <tejasbitale007@gmail.com>
2022-07-22 20:14:01 +0530

    deploy my react app to github

--- src/PortfolioContainer/ContactMe/Contact.js
+++ src/PortfolioContainer/ContactMe/Contact.js
@@ -26,10 +26,14 @@ function Contact() {
                     <form ref={form} onSubmit={sendEmail} className='c-form' >
                         <h1 className='c-form-h1'>Conatct Me</h1>
                         <label>Name</label> <br></br>
+
                         <input type="text" placeholder="Enter youre name" name="user_name" className='i1'></input><br></br>
+
                         <lable>Email</lable><br></br>
                         <input type="email" placeholder="Enter your email" name="user_email" className='i2'></input><br></br>
+
                         <input type="textarea" placeholder="Subject" name="user_sub"  className='i3'></input><br></br>
+                        
                         <textarea rows="5" placeholder="Message" name="message"></textarea><br></br>
                         <input type="submit" value="send" className='button'></input>
                         {done && "thankyou"}
